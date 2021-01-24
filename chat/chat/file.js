ddb92993dcf7b8b21bf916a92853bae340ac574e
grigoreme <grigore997@gmail.com>
2021-01-24 13:30:02 +0200

    minmax

--- chat/chat/file.js
+++ chat/chat/file.js
@@ -1,80 +1,102 @@
-
 function showChat() {
-  const chatFooter = document.getElementById('chat-footer');
-  const chatBody = document.getElementById('chat-body');
+    const chatFooter = document.getElementById('chat-footer');
+    const chatBody = document.getElementById('chat-body');
 
-  chatBody.className = "card-body";
-  chatFooter.className = "card-footer";
+    chatBody.className = "card-body";
+    chatFooter.className = "card-footer";
 
-  const btn = document.getElementById('btn-chat');
-  btn.setAttribute('onclick', 'hideChat()');
+    const btn = document.getElementById('btn-chat');
+    btn.setAttribute('onclick', 'hideChat()');
 
-  const card = document.getElementById("card");
-  card.setAttribute('class', 'card border-blue');
+    const card = document.getElementById("card");
+    card.setAttribute('class', 'card border-blue');
 
 }
+
 function hideChat() {
-  const chatFooter = document.getElementById('chat-footer');
-  const chatBody = document.getElementById('chat-body');
+    const chatFooter = document.getElementById('chat-footer');
+    const chatBody = document.getElementById('chat-body');
 
-  chatBody.className = chatBody.className + '  d-none';
-  chatFooter.className = chatFooter.className + '  d-none';
+    chatBody.className = chatBody.className + '  d-none';
+    chatFooter.className = chatFooter.className + '  d-none';
 
-  const btn = document.getElementById('btn-chat');
-  btn.setAttribute('onclick', 'showChat()');
+    const btn = document.getElementById('btn-chat');
+    btn.setAttribute('onclick', 'showChat()');
 
-  const card = document.getElementById("card");
-  card.setAttribute('class', 'card border-red');
+    const card = document.getElementById("card");
+    card.setAttribute('class', 'card border-red');
 
 }
 
 hideChat();
+
 function sum() {
-  const a = document.getElementById('leta').value;
-  const b = document.getElementById('letb').value;
-  let result = Number(a) + Number(b);
-  if (numarbun()) {
-    alert('numar bun s-a facut');
-  }
+    const a = document.getElementById('leta').value;
+    const b = document.getElementById('letb').value;
+    let result = Number(a) + Number(b);
+    if (numarbun()) {
+        alert('numar bun s-a facut');
+    }
 
-  // if (result >= 100) {
-  //   alert("100+");
-  // }
-  // else if (result >= 70) {
-  //   alert("70+");
-  // }
-  // else if (result >= 50) {
-  //   alert("50+");
-  // }
-  // else if (result >= 10) {
-  //   alert("10+");
-  // }
-  // else if (result >= 0) {
-  //   alert("0+")
-  // }
+    // if (result >= 100) {
+    //   alert("100+");
+    // }
+    // else if (result >= 70) {
+    //   alert("70+");
+    // }
+    // else if (result >= 50) {
+    //   alert("50+");
+    // }
+    // else if (result >= 10) {
+    //   alert("10+");
+    // }
+    // else if (result >= 0) {
+    //   alert("0+")
+    // }
 
-  // 
-  function esteParsauImpar(numar) {
-    if (numar % 2 === 0) {
-      alert("Număr par")
-    } else {
-      alert("Număr impar")
+    // 
+    function esteParsauImpar(numar) {
+        if (numar % 2 === 0) {
+            alert("Număr par")
+        } else {
+            alert("Număr impar")
+        }
     }
-  }
-  function laPutere(num, putere) {
-    return Math.pow(num, putere)
-  }
-  function minMax(num1, num2) {
-    if (num1 > num2) {
-      return num1
-    } else if (num2 > num1) {
-      return num2
+
+    function laPutere(num, putere) {
+        return Math.pow(num, putere)
     }
-  }
-  function minMax2(num1, num2, num3) {
-    if (num1 > num2 && num1 > num3) {
-      return num1
-    } else if (num2 > num3 && num2 > num1) {
-      return num2
+
+    function minMax(num1, num2) {
+        if (num1 > num2) {
+            return num1
+        } else if (num2 > num1) {
+            return num2
+        }
     }
-  }
+
+    function minMax2(num1, num2, num3) {
+        if (num1 > num2 && num1 > num3) {
+            return num1
+        } else if (num2 > num3 && num2 > num1) {
+            return num2
+        }
+    }
+
+    function minMax(min, max, num) {
+        if (min > max) {
+            return undefined
+
+        }
+        if (num < min) {
+
+            return min
+        }
+
+        if (num > max) {
+            return max
+        }
+        return num
+    }
+
+    const x = minMax(10, 100, 50)
