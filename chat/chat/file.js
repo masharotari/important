9aacf20c0097e86ce649fe29e0c6ca26a22af778
grigoreme <grigore997@gmail.com>
2021-02-05 17:54:35 +0200

    calcularea mediei

--- chat/chat/file.js
+++ chat/chat/file.js
@@ -152,3 +152,83 @@ function sum() {
         rezultat[i] = arr[i];
         rezultat[i + arr2.length] = arr2;
     }
+
+
+
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+    // afisati pe ecran doar variabilele pare utilizand for sau forEach
+    // ex: 2, 4, 6, 8 
+    // afisati pe ecran doar variabilele impare utilizand for sau forEach
+    // ex: 1, 3, 5, 7
+    // 
+    for (let i = 0; i < 8; i++) {
+        if (arr[i] % 2 === 0) {
+            console.log(arr[i] + '  ' + i + ' ');
+        };
+    };
+
+
+
+    const matematica = [4, 2, 3, 8, 7, 4, 8];
+    const fizica = [5, 5, 5, 5, 8, 7];
+    const istoria = [8, 8, 7, 10, 9, 10];
+    const informatica = [2, 2, 2, 2, 2, 10, 10, 10];
+
+    // calculati media pe fiecare obiect
+    // afisati rezultatul pe ecran
+
+    // afisati media finala pe obiecte
+    let sum = 0;
+    let avg_sum = 0;
+
+    let sum_fizica = 0;
+    let avg_fizica = 0;
+
+    let sum_istoria = 0;
+    let avg_istoria = 0;
+
+    let sum_informatica = 0;
+    let avg_informatica = 0;
+
+    let sum_media_obiecte = 0;
+
+
+    for (let i = 0; i < matematica.length; i++) {
+        if (matematica[i]) {
+            sum = sum + matematica[i];
+        }
+    };
+
+    avg_sum = sum / matematica.length;
+
+    for (let i = 0; i < fizica.length; i++) {
+        if (fizica[i]) {
+            sum_fizica = sum_fizica + fizica[i];
+        };
+    };
+
+    avg_fizica = sum_fizica / fizica.length;
+
+    for (let i = 0; i < istoria.length; i++) {
+        if (istoria[i]) {
+            sum_istoria = sum_istoria + istoria[i];
+        };
+    };
+    avg_istoria = sum_istoria / istoria.length;
+
+    for (let i = 0; i < informatica.length; i++) {
+        // sum += parseInt( informatica[i], 10 )
+        if (informatica[i]) {
+            sum_informatica = sum_informatica + informatica[i];
+        };
+    };
+    avg_informatica = sum_informatica / informatica.length;
+    const media_obiecte = [avg_sum, avg_fizica, avg_istoria, avg_informatica];
+
+
+    for (let i = 0; i < media_obiecte.length; i++) {
+        if (media_obiecte[i]) {
+            sum_media_obiecte = sum_media_obiecte + media_obiecte[i];
+        };
+    };
+    sum_media_obiecte = sum_media_obiecte / media_obiecte.length;
